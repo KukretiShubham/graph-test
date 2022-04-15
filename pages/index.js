c4a0b42871559440634bfb14dc05f5faabb0f837
Shubham Kukreti <shubhamkukreti204@gmail.com>
2022-04-15 20:42:33 +0530

    added: rechart graph

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { gql } from "@apollo/client";
 import client from "../lib/apollo-client";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 export default function Home({ data }) {
   console.log("totalAmounts: ", data);
@@ -13,9 +14,23 @@ export default function Home({ data }) {
         <h2>🦩 Howdie! My data is here</h2>
         {data.totalAmounts.length > 0 &&
           data.totalAmounts.map((item) => (
-            <div key={item.id}>
+            <>
+            {/* <div key={item.id}>
+              <br></br>
               <span>{item.id}</span>
-            </div>
+              <br></br>
+              <span>{item.amount}</span>
+            </div> */}
+              <LineChart width={600} height={300} data={[{name: item.id, uv: item.amount}]} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+              </LineChart>
+            </>
+
+            
           ))}
       </>
     );
